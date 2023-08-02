3847b1ba066f0223c03a4b2bbc9cb145df85dd06
ayodhya98 <75997356+ayodhya98@users.noreply.github.com>
2023-08-02 15:51:42 +0530

    Add project files.

---
+++ mexxar/Controllers/CategoryController.cs
@@ -0,0 +1,123 @@
+﻿using mexxar.Data;
+using mexxar.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mexxar.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly MexxarDbContext _db;
+      
+
+        public CategoryController(MexxarDbContext db)
+        {
+            _db = db;
+
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+
+        //Get
+        
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+
+        public IActionResult Edit(int? id)
+        {
+
+            if (id==null || id==0) 
+            {
+                return NotFound();
+            }
+           var categoryFromDb = _db.categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //Get
+
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteTodo(int? id)
+        {
+
+            var obj = _db.categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+
+            _db.categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+           
+        }
+
+
+
+
+
+    }
+}
---
+++ mexxar/Data/MexxarDbContext.cs
@@ -0,0 +1,14 @@
+﻿using mexxar.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mexxar.Data
+{
+    public class MexxarDbContext : DbContext
+    {
+        public MexxarDbContext(DbContextOptions<MexxarDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
